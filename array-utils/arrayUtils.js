3f86de3fb121268234416897cba42fb547c584e1
Ujwalcode24 <ujwalcode@gmail.com>
2020-12-21 16:48:01 +0530

    solved assignment

--- array-utils/arrayUtils.js
+++ array-utils/arrayUtils.js
@@ -1,7 +1,23 @@
 
-export {
-  forEach,
-  map,
-  filter,
-  reduce,
+const Numarr = [1,2,3,4,5,6,7,8,9,10];
+
+Array.prototype.ujwalMap = function(func){
+  let results = [];
+  for(let i = 0; i < this.length; i++){
+    results.push(func(this[i], i, this));
+  }
+  return results;
 }
+
+Numarr.forEach(num => num*2);
+
+
+
+
+
+// export {
+//   UjwalforEach,
+//   ujwalMap,
+//   Ujwalfilter,
+//   Ujwalreduce,
+// }
